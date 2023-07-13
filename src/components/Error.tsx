e2803597437411658949b368fdaf006ee3a52ba3
Meriç Mert Bulca <mericmertbulca@Merics-MBP.home>
2023-07-14 02:37:46 +0300

    error page

--- src/components/Error.tsx
+++ src/components/Error.tsx
@@ -1,8 +1,14 @@
 
 export default function ProfileError() {
-  return (
-    <div className="w-full h-screen flex justify-center items-center">
-        <h1 className="text-3xl">No User is found!</h1>
-    </div>
-  )
+    return (
+        <div className="w-full h-screen flex justify-center items-center">
+            <div className="text-container flex">
+                <h1 className="text-2xl ">
+                    404
+                    <span className="text-4xl mx-4 font-thin">|</span>
+                    <span className="text-[1rem]">No User is found</span>
+                </h1>
+            </div>
+        </div>
+    )
 }
